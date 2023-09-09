d856ed774ca0b12074f1279b1236b952ecaf50d1
ademesojosiah <josiahademeso@gmail.com>
2023-09-09 01:42:04 +0100

    initial commit

---
+++ src/main/java/com/jojo/testing/hogwartartfact/HogwartArtfactApplication.java
@@ -0,0 +1,13 @@
+package com.jojo.testing.hogwartartfact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartArtfactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartArtfactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojo/testing/hogwartartfact/HogwartArtfactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojo.testing.hogwartartfact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartArtfactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
